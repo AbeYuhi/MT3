0862eb53786bdfa34bdaca44e62317073ef18db7
AbeYuhi <k022g0032@g.neec.ac.jp>
2023-04-14 12:06:49 +0900

    学籍番号の変更

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <Novice.h>
 
-const char kWindowTitle[] = "学籍番号";
+const char kWindowTitle[] = "LE2A_02_アベユウヒ";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
