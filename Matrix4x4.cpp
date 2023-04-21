353a8cab187888aad52931cb6234e0f5676e64ca
AbeYuhi <k022g0032@g.neec.ac.jp>
2023-04-21 12:17:32 +0900

    Matrix4x4の「スケール、平行移動」の実装

--- Matrix4x4.cpp
+++ Matrix4x4.cpp
@@ -244,6 +244,41 @@ Matrix4x4 MakeIdentity4x4() {
 	return matrix;
 }
 
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate){
+	Matrix4x4 matrix = MakeIdentity4x4();
+	matrix.m[3][0] = translate.x;
+	matrix.m[3][1] = translate.y;
+	matrix.m[3][2] = translate.z;
+
+	return matrix;
+}
+
+Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
+	Matrix4x4 matrix = {};
+
+	matrix.m[0][0] = scale.x;
+	matrix.m[1][1] = scale.y;
+	matrix.m[2][2] = scale.z;
+	matrix.m[3][3] = 1;
+
+	return matrix;
+}
+
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix) {
+	Vector3 result = {};
+
+	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] + 1.0f * matrix.m[3][0];
+	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + 1.0f * matrix.m[3][1];
+	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + 1.0f * matrix.m[3][2];
+	float w  = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] + 1.0f * matrix.m[3][3];
+	assert(w != 0.0f);
+	result.x /= w;
+	result.y /= w;
+	result.z /= w;
+
+	return result;
+}
+
 void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
 	Novice::ScreenPrintf(x, y, "%s", label);
 	for (int row = 0; row < 4; row++) {
--- Matrix4x4.h
+++ Matrix4x4.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <Vector3.h>
+#include <cassert>
 
 struct Matrix4x4 {
 	float m[4][4];
@@ -17,5 +19,11 @@ Matrix4x4 Inverse(Matrix4x4 matrix);
 Matrix4x4 Transpose(Matrix4x4 matrix);
 
 Matrix4x4 MakeIdentity4x4();
+
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
+
+Matrix4x4 MakeScaleMatrix(const Vector3& scale);
+
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
 
 void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label);
